06b0898abd08616e628fc19deac05d42c162a14f
ginnikapoor <kapoorginni@gmail.com>
2015-09-29 14:25:47 +0530

    start

---
+++ src/com/join/join/fragment/DiscoverFragment.java
@@ -0,0 +1,45 @@
+package com.join.join.fragment;
+
+import com.example.materialdesign.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DiscoverFragment  extends Fragment {
+
+	public DiscoverFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_discover, container, false);
+
+
+		// Inflate the layout for this fragment
+		return rootView;
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+	}
+}
+
---
+++ src/com/join/join/fragment/MeMyFragment.java
@@ -0,0 +1,51 @@
+package com.join.join.fragment;
+
+import com.example.materialdesign.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+ 
+ 
+public class MeMyFragment extends Fragment {
+	
+	TextView txt1,txt2,txt3,txt4,txt5;
+	EditText ed1,ed2,ed3,ed4,ed5;
+	Button b1;
+ 
+    public MeMyFragment() {
+        // Required empty public constructor
+    }
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+    }
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_me_my, container, false);
+ 
+ 
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+ 
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+ 
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
